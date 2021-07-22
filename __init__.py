a0e29d13ab5ce5c93c8640642148592256b035bc
Duncan Zauss <Duncan.Zauss@gmx.net>
2021-07-22 11:24:06 +0200

    Restructure for setup.py install

--- __init__.py
+++ __init__.py
@@ -1,13 +1,2 @@
-import openpifpaf
+from .openpifpaf_hand import register
 
-from .freihand import Freihand
-from .rhd import RHD
-from .cifonly import CifOnly
-
-def register():
-    openpifpaf.DATAMODULES['freihand'] = Freihand
-    openpifpaf.DATAMODULES['rhd'] = RHD
-    openpifpaf.DECODERS.add(CifOnly)
-    openpifpaf.CHECKPOINT_URLS['shufflenetv2k16-hand'] = 'https://github.com/DuncanZauss/' \
-        'openpifpaf_assets/releases/download/v0.1.0/freihand_wholebody_sk16.pkl.epoch600'
-   
---
+++ openpifpaf_hand/__init__.py
@@ -0,0 +1,13 @@
+import openpifpaf
+
+from .freihand import Freihand
+from .rhd import RHD
+from .cifonly import CifOnly
+
+def register():
+    openpifpaf.DATAMODULES['freihand'] = Freihand
+    openpifpaf.DATAMODULES['rhd'] = RHD
+    openpifpaf.DECODERS.add(CifOnly)
+    openpifpaf.CHECKPOINT_URLS['shufflenetv2k16-hand'] = 'https://github.com/DuncanZauss/' \
+        'openpifpaf_assets/releases/download/v0.1.0/freihand_wholebody_sk16.pkl.epoch600'
+   
